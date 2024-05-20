a8ef86b8bbeedd23d509dab59410deab037ddb48
Dylan Werner-Meier <dylan.bloger@gmail.com>
2024-05-20 13:53:18 +0200

    move stuff

--- transformers/ner.js
+++ transformers/ner.js
@@ -1,8 +1,8 @@
 
 
-module.exports = function(RED) {
-      // Dynamically import the Transformers.js library
-  //let { pipeline, env } = await import('@xenova/transformers');
+module.exports = async function(RED) {
+    // Dynamically import the Transformers.js library
+    let { pipeline, env } = await import('@xenova/transformers');
 
     function NER(config) {
       RED.nodes.createNode(this,config);
