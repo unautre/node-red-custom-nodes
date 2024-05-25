7b49490250481cc96f429277cb54f18b77dafe9a
Dylan Werner-Meier <dylan.bloger@gmail.com>
2024-05-25 15:41:45 +0200

    rename and add config node

--- node-red-contrib-minio/minio.js
+++ node-red-contrib-minio/minio.js
@@ -1,17 +1,14 @@
 module.exports = function(RED) {
     "use strict";
     var fs = require('fs');
-    //var minimatch = require("minimatch");
     var minio = require("minio");
     var http = require("http");
     const { Readable } = require('stream');
 
-    function MinioPush(config) {
+    function MinioConfig(config) {
         RED.nodes.createNode(this, config);
-        var node = this;
 
         this.bucketName = config.bucketName;
-        this.prefix = config.prefix;
         this.minioClient = new minio.Client({
             endPoint: config.endPoint,
             port: config.port,
@@ -19,6 +16,18 @@ module.exports = function(RED) {
             accessKey: config.accessKey,
             secretKey: config.secretKey
         });
+    }
+
+    function MinioPush(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        this.bucketName = config.bucketName;
+        this.prefix = config.prefix;
+        
+        const bucket = RED.nodes.getNode(config.bucket);
+        this.minioClient = bucket.minioClient;
+        this.bucketName = bucket.bucketName;
 
         node.on('input', function(msg) {
             var filename = this.prefix || "";
@@ -47,5 +56,35 @@ module.exports = function(RED) {
         });
     }
 
-    RED.nodes.registerType("minio-push",MinioPush);
+    function MinioListen(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        this.bucketName = config.bucketName;
+        this.prefix = config.prefix || "";
+        this.suffix = config.suffix || "";
+        this.events = [minio.ObjectCreatedAll];
+        
+        const bucket = RED.nodes.getNode(config.bucket);
+        this.minioClient = bucket.minioClient;
+        this.bucketName = bucket.bucketName;
+
+        this.poller = this.minioClient.listenBucketNotification(this.bucketName, this.prefix, this.suffix, this.events);
+        this.poller.on("notification", function(record) {
+            const bucketName = record.s3.bucket.name;
+            const objectKey = record.s3.object.key;
+            node.emit("input", {payload: record})
+        });
+
+        node.on('input', function(msg) {
+            node.send(msg);
+        });
+        node.on('close', function() {
+            this.poller.stop();
+        })
+    }
+
+    RED.nodes.registerType("minio-config",MinioConfig);
+    RED.nodes.registerType("minio-push", MinioPush);
+    RED.nodes.registerType("minio-listen", MinioListen);
 }
