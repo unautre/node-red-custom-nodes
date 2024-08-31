65bc94fcce90d6fbf87160fb44d1b2bf9be51e2a
Dylan Werner-Meier <dylan.bloger@gmail.com>
2024-08-31 19:51:30 +0200

    add infos on minio pull

--- node-red-contrib-minio/minio.js
+++ node-red-contrib-minio/minio.js
@@ -83,7 +83,7 @@ module.exports = function(RED) {
             const buf = await buffer(stream);
 
             node.status({fill:"blue",shape:"dot",text:"Read file " + filename + " !"});
-            return {payload: buf};
+            return {payload: buf, bucket:this.bucketName, key:filename};
         });
     }
 
